765ff6cc1734fe3d59f7d1611884d19f5f3c5b49
Dmitriy Khliebnikov <dimatello2007@mail.ru>
2018-05-27 16:25:26 +0300

    урок 9

--- engine/CMS.php
+++ engine/CMS.php
@@ -2,6 +2,7 @@
 
 namespace Engine;
 
+use Engine\Core\Router\DispatchedRoute;
 use Engine\Helper\Common;
 
 class CMS {
@@ -26,20 +27,28 @@ class CMS {
      */
     public function run()
     {
-     //   echo 'Hello:)<BR>';
-        $this->router->add('home','/','HomeController:index');
-        $this->router->add('news','/news/','HomeController:news');
-        $this->router->add('user','/user/','UserController:index');
-       // print_r($this->di);
+    try {
+        $this->router->add('home', '/', 'HomeController:index');
+        $this->router->add('news', '/news/', 'HomeController:news');
+        $this->router->add('user', '/user/', 'UserController:index');
+        // print_r($this->di);
 
-       $routerDispatch = $this->router->dispatch(common::getMethod(),common::getPathUrl());
+        $routerDispatch = $this->router->dispatch(common::getMethod(), common::getPathUrl());
+        if ($routerDispatch == null) {
+            $routerDispatch = new DispatchedRoute('ErrorController:page404');
+        }
 
-       list($class, $action) = explode(':',$routerDispatch->getController(),2);
-        $controller = '\\Cms\\Controller\\'.$class;
-       call_user_func_array([new $controller($this->di), $action],$routerDispatch->getParameters());
-       // print_r($class);        print_r($action);
-      //  print_r($routerDispatch);
-     // echo common::getMethod();
-     //  echo common::getPathUrl();
+        list($class, $action) = explode(':', $routerDispatch->getController(), 2);
+        $controller = '\\Cms\\Controller\\' . $class;
+        $parameters = $routerDispatch->getParameters();
+        call_user_func_array([new $controller($this->di), $action], $parameters);
+        // print_r($class);        print_r($action);
+        //  print_r($routerDispatch);
+        // echo common::getMethod();
+        //  echo common::getPathUrl();
+    } catch (\Exception $e) {
+            echo $e->getMessage();
+            exit;
+            }
     }
 }
